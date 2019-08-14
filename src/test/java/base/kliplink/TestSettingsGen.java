f3690baa9712ababbfa95f95c6460f6ff0491239
Kamran Kashef <kamrankashef@users.noreply.github.com>
2019-08-13 22:44:53 -0400

    Expand test coverage with unit-like tests. Addition of StringUtil to exported artifacts.
    2 tests exposing breaks have been added.

---
+++ src/test/java/base/kliplink/TestSettingsGen.java
@@ -0,0 +1,109 @@
+package base.kliplink;
+
+import base.application.ApplicationBuilder;
+import base.dl.methodgenerators.FromRs;
+import base.dl.methodgenerators.GetByGuid;
+import base.dl.methodgenerators.HardDeleteByGuid;
+import base.dl.methodgenerators.InsertObject;
+import base.dl.methodgenerators.InsertRaw;
+import base.gen.DLGen;
+import base.gen.ModelGen;
+import base.model.AbstractModel;
+import base.model.ForiegnKeyField;
+import base.model.Model;
+import base.model.PrimitiveField;
+import base.model.PrimitiveType;
+import base.model.methodgenerators.ConstructorGenerator;
+import base.model.methodgenerators.DerivedModelConstructorGenerator;
+import base.model.methodgenerators.ToStringGenerator;
+import base.parsergen.rules.ModelAugmenterI;
+import base.parsergen.rules.ModelTransformerI;
+import base.util.FileUtil;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+import org.junit.Test;
+
+// TODO This does not perform any assertions - compare generated assets to expected results
+public class TestSettingsGen {
+
+    private static final String ORG = "com.kliplink";
+    private static final String EXPORT_DIR = "/tmp/kliplink_gen";
+
+    @Test
+    public void testSettingsGen() throws IOException {
+        FileUtil.deleteDir(new File(EXPORT_DIR));
+        final Map<String, AbstractModel> modelMap = new HashMap<>();
+
+        final AbstractModel org = new Model("Org", ORG);
+
+        org.addPrimitiveField(new PrimitiveField("Site", PrimitiveType.TINY_TEXT, false));
+        org.addPrimitiveField(new PrimitiveField("FirstName", PrimitiveType.TINY_TEXT, false));
+        org.addPrimitiveField(new PrimitiveField("LastName", PrimitiveType.TINY_TEXT, false));
+        org.addPrimitiveField(new PrimitiveField("Email", PrimitiveType.TINY_TEXT, false));
+
+        final AbstractModel settings = new Model("Setting", ORG);
+        settings.addPrimitiveField(new PrimitiveField("SettingName", PrimitiveType.TINY_TEXT, false));
+        settings.addPrimitiveField(new PrimitiveField("DataValue", PrimitiveType.TINY_TEXT, false));
+        settings.addPrimitiveField(new PrimitiveField("DataType", PrimitiveType.TINY_TEXT, false));
+        settings.addFKField(new ForiegnKeyField(Collections.EMPTY_SET, org, true));
+
+        final AbstractModel creds = new Model("Cred", ORG);
+        creds.addPrimitiveField(new PrimitiveField("AccessKey", PrimitiveType.TINY_TEXT, false));
+        creds.addPrimitiveField(new PrimitiveField("SecretKey", PrimitiveType.TINY_TEXT, false));
+        creds.addPrimitiveField(new PrimitiveField("type", PrimitiveType.TINY_TEXT, false));
+        creds.addFKField(new ForiegnKeyField(Collections.EMPTY_SET, org, true));
+
+        modelMap.put(org.getJavaClassName(), org);
+        modelMap.put(settings.getJavaClassName(), settings);
+        modelMap.put(creds.getJavaClassName(), creds);
+
+        makeStandardSet(modelMap);
+    }
+
+    private static void makeStandardSet(
+            final Map<String, AbstractModel> modelMap) throws IOException {
+
+        final Set<ModelGen.ModelMethodGenerator> mergedModelMethods = new HashSet<>();
+        mergedModelMethods.add(new DerivedModelConstructorGenerator());
+        mergedModelMethods.add(new ConstructorGenerator());
+        mergedModelMethods.add(new ToStringGenerator());
+
+        final Set<DLGen.DLMethodGenerator> mergedDLMethods = new LinkedHashSet<>();
+
+        mergedDLMethods.add(new InsertObject());
+        mergedDLMethods.add(new FromRs());
+        mergedDLMethods.add(new GetByGuid());
+        mergedDLMethods.add(new HardDeleteByGuid()); // Need to change to delete by special keys
+        mergedDLMethods.add(new InsertRaw());
+
+        for (final AbstractModel model : modelMap.values()) {
+            for (final DLGen.DLMethodGenerator dlGen : mergedDLMethods) {
+                model.addDLMethodGenerator(dlGen);
+            }
+            for (final ModelGen.ModelMethodGenerator methodGen : mergedModelMethods) {
+                model.addModelMethodGenerator(methodGen);
+            }
+        }
+
+        final Map<String, String> additionalJavaFiles = Collections.EMPTY_MAP;
+
+        final String mainsBuildXML = "";
+
+        ApplicationBuilder.buildElementParserAndLayerModels(ORG,
+                modelMap,
+                additionalJavaFiles,
+                ModelAugmenterI.EMPTY_AUGMENTER,
+                ModelTransformerI.EMPTY_TRANSFORMER,
+                mergedModelMethods,
+                mergedDLMethods,
+                mainsBuildXML,
+                EXPORT_DIR);
+    }
+}
+
---
+++ src/test/java/base/util/CaseConversionTest.java
@@ -0,0 +1,96 @@
+package base.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class CaseConversionTest {
+
+    @Test
+    public void isUpperTest() {
+        assertFalse(CaseConversion.isUpper("1"));
+        assertTrue(CaseConversion.isDigit("1"));
+    }
+
+    @Test
+    public void testToJavaVarName() {
+        assertEquals("toi3EmptyNetEv", CaseConversion.toJavaVariableName("TOI3EmptyNetEV"));
+
+        assertEquals("hi", CaseConversion.toJavaVariableName("Hi"));
+        assertEquals("isTempId", CaseConversion.toJavaVariableName("IsTempID"));
+        assertEquals("id", CaseConversion.toJavaVariableName("ID"));
+        assertEquals("i", CaseConversion.toJavaVariableName("I"));
+        assertEquals("id", CaseConversion.toJavaVariableName("ID"));
+        assertEquals("bye", CaseConversion.toJavaVariableName("bye"));
+        assertEquals("fooBar", CaseConversion.toJavaVariableName("fooBar"));
+        assertEquals("bye", CaseConversion.toJavaVariableName("BYE"));
+        assertEquals("fName", CaseConversion.toJavaVariableName("FName"));
+        assertEquals("trIck", CaseConversion.toJavaVariableName("TRIck"));
+        assertEquals("faceoffs3v3Won", CaseConversion.toJavaVariableName("faceoffs3v3Won"));
+        assertEquals("toiEmptyNetEv", CaseConversion.toJavaVariableName("TOIEmptyNetEV"));
+        assertEquals("homeCoachFName", CaseConversion.toJavaVariableName("homeCoachFName"));
+
+        assertEquals("assistaId", CaseConversion.toJavaVariableName("AssistaID"));
+        assertEquals("assist1Id", CaseConversion.toJavaVariableName("Assist1ID"));
+        assertEquals("toi4v5Aug", CaseConversion.toJavaVariableName("TOI4v5Aug"));
+
+        assertEquals("fooX", CaseConversion.toJavaVariableName("FooX"));
+        assertEquals("fooX", CaseConversion.toJavaVariableName("fooX"));
+    }
+
+    @Test
+    public void toJavaClassName() {
+        assertEquals("Ab", CaseConversion.toJavaClassName("ab"));
+        assertEquals("Ab", CaseConversion.toJavaClassName("AB"));
+        assertEquals("I", CaseConversion.toJavaClassName("I"));
+        assertEquals("I", CaseConversion.toJavaClassName("i"));
+        assertEquals("IFoo", CaseConversion.toJavaClassName("i_foo"));
+        assertEquals("PointsAgainstPg", CaseConversion.toJavaClassName("Points_Against_PG"));
+        assertEquals("HsState", CaseConversion.toJavaClassName("HS_state"));
+        assertEquals("EventNum", CaseConversion.toJavaClassNameUnunderscore("Event_Num"));
+        assertEquals("EventNum", CaseConversion.toJavaClassNameUnunderscore("EventNum"));
+    }
+
+    @Test
+    public void testToDBColName() {
+
+        assertEquals("ab", CaseConversion.toDBName("Ab"));
+        assertEquals("hi", CaseConversion.toDBName("Hi"));
+        assertEquals("i", CaseConversion.toDBName("i"));
+        assertEquals("is_temp_id", CaseConversion.toDBName("IsTempID"));
+        assertEquals("id", CaseConversion.toDBName("ID"));
+        assertEquals("bye", CaseConversion.toDBName("bye"));
+        assertEquals("foo_bar", CaseConversion.toDBName("fooBar"));
+        assertEquals("bye", CaseConversion.toDBName("BYE"));
+        assertEquals("f_name", CaseConversion.toDBName("FName"));
+        assertEquals("tr_ick", CaseConversion.toDBName("TRIck"));
+        assertEquals("faceoffs3v3_won", CaseConversion.toDBName("faceoffs3v3Won"));
+        assertEquals("toi_empty_net_ev", CaseConversion.toDBName("TOIEmptyNetEV"));
+        assertEquals("home_coach_f_name", CaseConversion.toDBName("homeCoachFName"));
+        assertEquals("first__second", CaseConversion.toDBName("First_Second"));
+
+        assertEquals("hit__ab", CaseConversion.toDBName("Hit_Ab"));
+        assertEquals("first__sx", CaseConversion.toDBName("First_Sx"));
+        assertEquals("first__sx", CaseConversion.toDBName("First_Sx"));
+        assertEquals("first__t__blah", CaseConversion.toDBName("First_T_Blah"));
+        assertEquals("first__t", CaseConversion.toDBName("First_T"));
+        assertEquals("first__sx", CaseConversion.toDBName("First_Sx"));
+        assertEquals("first_sx", CaseConversion.toDBName("First_sx"));
+        assertEquals("firs_t__sx", CaseConversion.toDBName("Firs_t_Sx"));
+        assertEquals("firs__t__sx", CaseConversion.toDBName("Firs_T_Sx"));
+
+        assertEquals("first__s", CaseConversion.toDBName("First_S"));
+        assertEquals("points_against_pg", CaseConversion.toDBName("Points_Against_PG"));
+        assertEquals("hs_state", CaseConversion.toDBName("HS_state"));
+    }
+
+    @Test
+    public void testFirstToUpper() {
+        assertEquals("Bye", CaseConversion.firstLetterUp("bye"));
+        assertEquals("A", CaseConversion.firstLetterUp("a"));
+
+    }
+}
+
