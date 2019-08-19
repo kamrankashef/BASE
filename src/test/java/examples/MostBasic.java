567d480ee2ab67a79935cb162c1e5e6ff9bb9edf
Kamran Kashef <kamrankashef@users.noreply.github.com>
2019-08-18 20:03:36 -0400

    Build out MostBasic example

--- src/test/java/examples/MostBasic.java
+++ src/test/java/examples/MostBasic.java
@@ -1,27 +1,40 @@
 package examples;
 
 import base.parsergen.rules.SourceFiles;
+import base.v3.AbstractApplicationBuilder;
+import base.v3.XMLApplicationBuilder;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Not too interesting, because we are doing the very minimum and not specifying model and datalayer methods to generate
+ */
 public class MostBasic {
 
 
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-    // User prrovide path to source file
-    public static void main(String[] args) throws FileNotFoundException {
-
-        // 1. Get sample data file
+        // The only three required properties
+        // 1. org - package for generated classes
+        // 2. exportDir - where project should be generated - careful, this will wipe out the directory, so don't use $HOME
+        // 3. sourceFiles - StreamGetters that provide InputStreams to the sample datafiles
+        final String org = "com.example";
+        final String exportDir = System.getProperty("user.home") + "/sample_base_project";
         final SourceFiles sourceFiles = new SourceFiles();
 
         {
             final String pathToInputFile = args[0];
             SourceFiles.StreamGetter streamGetter = () -> new FileInputStream(pathToInputFile);
-            sourceFiles.addSourceFile("foo", streamGetter);
+            sourceFiles.addSourceFile("ExampleType", streamGetter);
         }
 
-        // 2. Decide
+        // Create an XML builder
+        final AbstractApplicationBuilder appBuilder = new XMLApplicationBuilder("com.example", sourceFiles, exportDir);
+
+        appBuilder.build();
+        System.out.println("In '" + exportDir + "' perform mvn compile");
     }
 }
