75fc952c0fa4b4bc7aa78aa562b811ce253dcb25
Kamran Kashef <kamrankashef@users.noreply.github.com>
2019-08-28 20:44:47 -0400

    Restore file

---
+++ src/test/java/fullsuite/XMLGenTest.java
@@ -0,0 +1,92 @@
+package fullsuite;
+
+import base.dl.methodgenerators.InsertObject;
+import base.dl.methodgenerators.InsertRaw;
+import base.model.methodgenerators.AttributeBasedFromElemMethodGenerator;
+import base.model.methodgenerators.ConstructorGenerator;
+import base.model.methodgenerators.DerivedModelConstructorGenerator;
+import base.parsergen.rules.SourceFiles;
+import base.v3.AbstractApplicationBuilder;
+import base.v3.XMLApplicationBuilder;
+import base.workflow.Helpers;
+import org.junit.Test;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+
+public abstract class XMLGenTest {
+
+
+    protected abstract String getBaselineDir();
+
+    protected abstract String getYAMLSource();
+
+    protected abstract void applyOverrides(AbstractApplicationBuilder abstractApplicationBuilder);
+
+    protected String getExportDir() {
+        return "/tmp/project_test_out";
+    }
+
+    protected String getOrg() {
+        return "no.org.set";
+    }
+
+    protected SourceFiles getSourceFiles() throws IOException {
+        final String yamlAsString = Helpers.resourceAsString(getClass(), getYAMLSource());
+        System.out.println("YAML String is " + yamlAsString);
+        final Map<String, Object> config = (Map<String, Object>) new Yaml().load(yamlAsString);
+        final String rootDir = (String) config.get("rootDir");
+        final SourceFiles sourceFiles = new SourceFiles(rootDir);
+
+        for (final Map<String, String> sourceFileAsMap : (Collection<Map<String, String>>) config.get("sourceFiles")) {
+
+            final String fileName = sourceFileAsMap.get("fileName");
+            final String type = sourceFileAsMap.get("type");
+
+            sourceFiles.addSourceFile(type, () -> {
+                final String resourcePath = rootDir + "/" + fileName;
+                System.out.println("Resource path is " + resourcePath);
+                return XMLGenTest.class.getClassLoader().getResourceAsStream(resourcePath);
+            });
+        }
+
+        return sourceFiles;
+    }
+
+
+    protected AbstractApplicationBuilder getApplicationBuilder() throws IOException {
+        return new XMLApplicationBuilder(getOrg(), getSourceFiles(), getExportDir());
+    }
+
+    @Test
+    final public void runTest() throws FileNotFoundException, InterruptedException, IOException {
+
+        final AbstractApplicationBuilder abstractApplicationBuilder
+                = getApplicationBuilder()
+                .addMergedModelMethod(new ConstructorGenerator())
+                .addElemModelMethods(new AttributeBasedFromElemMethodGenerator())
+                .addElemModelMethods(new ConstructorGenerator())
+                .addMergedModelMethod(new DerivedModelConstructorGenerator())
+                .addMergedDLModelMethod(new InsertRaw())
+                .addMergedDLModelMethod(new InsertObject());
+
+
+        applyOverrides(abstractApplicationBuilder);
+
+        abstractApplicationBuilder.build();
+
+        final String baselineDir = getBaselineDir();
+
+        // if (!getAutoGenTypeSet()) {
+        // Previously had switch in for discovery mode
+        // should expose this via something like abstractApplicationBuilder.analyze()
+        FullSuite.runDiff(getBaselineDir(), getExportDir());
+        //}
+    }
+
+}
+
